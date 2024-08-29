c08236df99dd1fca29570711239dc98791422d44
Fernando Junior <fernandobfr10@gmail.com>
2024-08-29 13:47:35 -0300

    feat: add sonner lib for toasts

--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -5,6 +5,7 @@ import { z } from "zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { toast } from "sonner";
 
 const sigInFormSchema = z.object({
   email: z.string().email()
@@ -16,8 +17,15 @@ export function Signin() {
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<SignInForm>()
 
   async function handleSignIn(data: SignInForm) {
-    console.log(data)
-    await new Promise(resolve => setTimeout(resolve, 2000))
+    try {
+      console.log(data)
+      await new Promise(resolve => setTimeout(resolve, 2000))
+      toast.success('Enviamos um link de autenticação para seu e-mail.', {
+        action: { label: 'Reenviar', onClick: () => handleSignIn(data) }
+      })
+    } catch (error) {
+      toast.error('Credenciais inválidas.')
+    }
   }
 
   return (
