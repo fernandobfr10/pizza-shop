1b3c08d0ea29c2e3f9aa1a7d4858a48f65180a51
Fernando Junior <fernandobfr10@gmail.com>
2024-08-29 12:14:40 -0300

    feat: add default layouts using outlet

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 
+import { AppLayout } from "@/pages/_layouts/App";
+import { AuthLayout } from "@/pages/_layouts/Auth";
 import { Dashboard } from "@/pages/app/Dashboard";
 import { Signin } from "@/pages/auth/Signin";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <Dashboard />},
-  { path: "/sign-in", element: <Signin />}
+  { 
+    path: '/', 
+    element: <AppLayout />, 
+    children: [{ path: '/', element: <Dashboard />}]
+  },
+  { 
+    path: '/sign-in', 
+    element: <AuthLayout />, 
+    children: [{ path: '/sign-in', element: <Signin />}]
+  },
 ])
