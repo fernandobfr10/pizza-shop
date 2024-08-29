ae7677a1b9072d8e769214fa2d59ee0e30dd10fb
Fernando Junior <fernandobfr10@gmail.com>
2024-08-29 15:46:51 -0300

    feat: create table component and filters component

---
+++ src/pages/app/orders/OrderTableRow.tsx
@@ -0,0 +1,47 @@
+import { ArrowRight, Search, X } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { TableCell, TableRow } from "@/components/ui/table";
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="size-3" />
+          <span className="sr-only">Detalhes do pedido</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        1234567890
+      </TableCell>
+      <TableCell className="text-muted-foreground">
+        há 15 minutos
+      </TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="size-2 rounded-full bg-slate-400" />
+          <span>Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">
+        Fernando Junior
+      </TableCell>
+      <TableCell className="font-medium">
+        R$ 159,90
+      </TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="size-3 mr-2" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="size-3 mr-2" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,10 @@ import { createBrowserRouter } from "react-router-dom";
 
 import { AppLayout } from "@/pages/_layouts/App";
 import { AuthLayout } from "@/pages/_layouts/Auth";
+
 import { Dashboard } from "@/pages/app/Dashboard";
+import { Orders } from "@/pages/app/orders/Orders";
+
 import { SignIn } from "@/pages/auth/SignIn";
 import { SignUp } from "@/pages/auth/SignUp";
 
@@ -10,7 +13,10 @@ export const router = createBrowserRouter([
   { 
     path: '/', 
     element: <AppLayout />, 
-    children: [{ path: '/', element: <Dashboard />}]
+    children: [
+      { path: '/', element: <Dashboard />},
+      { path: '/orders', element: <Orders />}
+    ]
   },
   { 
     path: '/', 
