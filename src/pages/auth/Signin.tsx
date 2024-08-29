16b1cdc37c9d95622817ca0ad1437d38ced108b6
Fernando Junior <fernandobfr10@gmail.com>
2024-08-29 12:55:04 -0300

    feat: update auth layout and update theme for red

--- src/pages/auth/Signin.tsx
+++ src/pages/auth/Signin.tsx
@@ -1,7 +1,28 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Helmet } from "react-helmet-async";
+
 export function Signin() {
   return (
-    <div>
-      <h1>Signin</h1>
-    </div>
+    <>
+      <Helmet title="Login" />
+     <div className="p-8">
+      <div className="w-[350px] flex flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">Acessar painel</h1>
+          <p className="text-sm text-muted-foreground">Acompanhe suas vendas pelo painel do parceiro.</p>
+        </div>
+
+        <form className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input type="email" id="email" />
+          </div>
+          <Button type="submit" className="w-full">Acessar painel</Button>
+        </form>
+      </div>
+     </div>
+    </>
   );
 }
